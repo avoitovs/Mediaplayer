fb01cb02b15308d5c65c90bc42932838e0f73998
Antons Voitovs <antooshaa@gmail.com>
2017-12-01 22:48:54 +0200

    asd

--- app/src/main/java/com/example/antons/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/antons/mediaplayer/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -13,15 +15,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     MediaPlayer player;
+    Button stop;
+    Button pause;
+    Button play;
+
+  //  TextView info = findViewById(R.id.songInfo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         play = findViewById(R.id.play);
+         stop = findViewById(R.id.stop);
+         pause = findViewById(R.id.pause);
+        ImageView coverImage = findViewById(R.id.cover);
 
+        coverImage.setImageResource(R.drawable.cover);
 
-        Button play = findViewById(R.id.play);
-        Button stop = findViewById(R.id.stop);
+        stop.setEnabled(false);
+        pause.setEnabled(false);
+
 
         player = MediaPlayer.create(this,R.raw.song);
 
@@ -29,26 +42,41 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Playing song",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Playing song",Toast.LENGTH_SHORT).show();
                 player.start();
+                stop.setEnabled(true);
+                pause.setEnabled(true);
             }
         });
 
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Stopping song",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Stopping song",Toast.LENGTH_SHORT).show();
                 player.stop();
                 try {
-                pre();} catch (IOException e){
+                preparePlayerToStart();
+                } catch (IOException e){
                     e.printStackTrace();
                 }
             }
         });
 
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Song paused",Toast.LENGTH_SHORT).show();
+                player.pause();
+            }
+        });
+
+
+
+
+
     }
 
-    private void pre () throws IOException{
+    private void preparePlayerToStart() throws IOException{
         player.prepare();
     }
 }
